8cd7b82259262ada45a8ec71ce001db7a9ff43ae
cmpgamer <bmnosar@gmail.com>
2022-08-15 10:34:44 -0400

    Initial Push

---
+++ client/vue-project/vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tester: resolve(__dirname, 'test.html')
+      }
+    },
+    emptyOutDir: true,
+    outDir: "../../server/dist"
+  }
+});
